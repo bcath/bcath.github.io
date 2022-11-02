f228473e086a5cf5d7aaaeda869c927b5f86e8fd
bcath <82913711+bcath@users.noreply.github.com>
2022-11-02 15:27:54 -0400

    change pictures

--- cardGame-MeVersion/app.js
+++ cardGame-MeVersion/app.js
@@ -1,62 +1,63 @@
 const cardsArray =[
     {
-        name: "cheeseburger",
-        img: "images/cheeseburger.png"
+        name: "spark",
+        img: "images/spark.png"
 
     },
     {
-        name: "fries",
-        img: "images/fries.png"
+        name: "light",
+        img: "images/light.png"
 
     },
     {
-        name: "hotdog",
-        img: "images/hotdog.png"
+        name: "heart",
+        img: "images/heart.png"
 
     },
     {
-        name: "ice-cream",
-        img: "images/ice-cream.png"
+        name: "ball",
+        img: "images/ball.png"
 
     },
     {
-        name: "milkshake",
-        img: "images/milkshake.png"
+        name: "stars",
+        img: "images/stars.png"
+
 
     },
     {
-        name: "pizza",
-        img: "images/pizza.png"
+        name: "moon",
+        img: "images/moon.png"
 
     },
     {
-        name: "cheeseburger",
-        img: "images/cheeseburger.png"
+        name: "spark",
+        img: "images/spark.png"
 
     },
     {
-        name: "fries",
-        img: "images/fries.png"
+        name: "light",
+        img: "images/light.png"
 
     },
     {
-        name: "hotdog",
-        img: "images/hotdog.png"
+        name: "heart",
+        img: "images/heart.png"
 
     },
     {
-        name: "ice-cream",
-        img: "images/ice-cream.png"
+        name: "ball",
+        img: "images/ball.png"
 
     },
     {
-        name: "milkshake",
-        img: "images/milkshake.png"
+        name: "stars",
+        img: "images/stars.png"
 
     },
     {
-        name: "pizza",
-        img: "images/pizza.png"
+        name: "moon",
+        img: "images/moon.png"
 
     }
 
